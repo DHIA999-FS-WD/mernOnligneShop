69b9a9828115c374deb19a78aaa0ed19c1b338ae
unknown <bouzaieneb52@gmail.com>
2023-11-28 13:47:12 +0100

    secondt commit

---
+++ backend/routes/productRoute.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const {
+  createProduct,
+  getProducts,
+  getProduct,
+  deleteProduct,
+  updateProduct,
+} = require("../controllers/productController");
+
+const router = express.Router();
+
+const protect = require("../middlewares/authMiddleware");
+
+// POST :  ADD A NEW Product TO THE DATABASE
+router.post("/add", protect, createProduct);
+//   GET :  RETURN ALL Product
+router.get("/all", protect, getProducts);
+//   GET :  RETURN ONE Product BY ID
+router.get("/:id", protect, getProduct);
+//   delete :  delete ONE Product BY ID
+router.delete("/:id", protect, deleteProduct);
+//  PUT : EDIT AND UPDATE A Product BY ID
+router.put("/:_id", protect, updateProduct);
+
+module.exports = router;
